b91725d8b6df1e6225338fd3e4c7d1dcfa533120
Aiden <bokjaeoh@gmail.com>
2023-06-23 18:35:47 +0900

    Add `applyConfig` method for custom configuration

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 /**
- * Configuration object to use when configuring a server using the ALExp library.
+ * Configuration object to use when configuring a server using the `ALExp` library.
  *
  * @namespace
- * @property {Number} port Port to listen on the Express server.
+ * @property {Number} port Port to listen on the `Express` server.
  */
 const Config = {
   port: 80
--- src/Server.js
+++ src/Server.js
@@ -1,12 +1,37 @@
 import express from 'express'
 import http from 'http'
 
+import Config from './Config.js'
+
+/**
+ * Returns an `ALExp` server configuration object(`Config`) that applied the user-defined configuration object.
+ *
+ * @param {Config} [custom={}] User-defined configuration.
+ * @param {Config} [original=Config] Default configuration.
+ * @returns {Config} Custom configuration.
+ */
+const applyConfig = (custom = {}, original = Config) => {
+  for (const key in original) {
+    if (original[key]?.constructor.name === 'Object') {
+      custom[key] = applyConfig(custom[key] || {}, original[key])
+    } else {
+      custom[key] = custom[key] !== undefined ? custom[key] : original[key]
+    }
+  }
+
+  return custom
+}
+
 /**
  * Create and return `Express` server application.
  *
+ * @param {Config} [config] Configuration object to use when creating an 'Express' server application.
  * @returns {Express} `Express` server application instance.
  */
-const create = () => {
+const create = (config) => {
+  config = (config && applyConfig(config)) || Config
+  // TODO: Logging configuration
+
   const app = express()
   // TODO: Express settings
 
