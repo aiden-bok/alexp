a9558254afdd116fc925f058c88d8649df85b39c
Aiden <bokjaeoh@gmail.com>
2023-06-23 17:04:45 +0900

    Change config module case

--- src/Config.js
+++ src/Config.js
@@ -2,10 +2,10 @@
  * Configuration object to use when configuring a server using the ALExp library.
  *
  * @namespace
- * @property {Number} port - Port to listen on the Express server.
+ * @property {Number} port Port to listen on the Express server.
  */
-const config = {
+const Config = {
   port: 80
 }
 
-export default config
+export default Config
