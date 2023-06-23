0372f597c25c0eb3e7c81995b769208096d3ed8e
Aiden <bokjaeoh@gmail.com>
2023-06-23 15:47:39 +0900

    Add configuration object to use when configuring server using ALExp

---
+++ src/config.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration object to use when configuring a server using the ALExp library.
+ *
+ * @namespace
+ * @property {Number} port - Port to listen on the Express server.
+ */
+const config = {
+  port: 80
+}
+
+export default config
