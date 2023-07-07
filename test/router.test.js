7cabca00c9270cfe87c6721b9c4602764f0cac2e
Aiden <bokjaeoh@gmail.com>
2023-07-07 16:21:01 +0900

    Add test for router of server application with configuration option

--- test/router.test.js
+++ test/router.test.js
@@ -5,7 +5,7 @@ jest.useFakeTimers()
 
 import alexp from '../src/index.js'
 
-describe('Test for router of server application', () => {
+describe('Test for router of server application with setRouter method', () => {
   let app = null
 
   beforeAll(async () => {
@@ -16,6 +16,32 @@ describe('Test for router of server application', () => {
       res.send('Route for test')
     })
     app.setRouter(api)
+  })
+
+  afterAll(async () => {
+    app.server.close()
+  })
+
+  test('Should response to test router request', () => {
+    return request(app)
+      .get('/test')
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('Route for test')
+      })
+  })
+})
+
+describe('Test for router of server application with configuration option', () => {
+  let app = null
+
+  beforeAll(async () => {
+    const api = new alexp.server.Router()
+    api.get('/test', (req, res) => {
+      res.send('Route for test')
+    })
+
+    app = alexp.server.create({ server: { router: api } })
   })
 
   afterAll(async () => {
